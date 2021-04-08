097b6cf4d265c1be4bfb780e7af2e0534850634d
christo <80416267+christo-blip@users.noreply.github.com>
2021-04-08 16:46:26 +0530

    This is the front end of the fetch file

---
+++ displayvid.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<form action="backup1.php" method="post" enctype="multipart/form-data">
+Batch
+<input type="text" name="Batch" id="n33">
+<input type="submit" name="search" value="search"> 
+</form>
+
+
+</body>
+</html>
